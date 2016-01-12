4f479131daeb585ed477a7a4ea4adde361a0d20f
langpingxue <langping.xue@activenetwork.com>
2016-01-12 16:59:43 +0800

    ok

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,48 @@
+module SessionsHelper
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def remember(user)
+	    user.remember
+	    cookies.permanent.signed[:user_id] = user.id
+	    cookies.permanent[:remember_token] = user.remember_token
+	    # debugger
+	end
+	# def current_user
+ #    	@current_user ||= User.find_by(id: session[:user_id])
+ #  	end
+   def current_user
+   	# puts "current_user"
+     if (user_id = session[:user_id])
+     	# puts "if user_id #{user_id}"
+        @current_user ||= User.find_by(id: user_id)
+     elsif (user_id = cookies.signed[:user_id])
+     	# puts "else user_id #{user_id}"
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+         log_in user
+         @current_user = user
+        end
+     end
+  end
+
+  	def logged_in?
+    	!current_user.nil?
+  	end
+
+  def forget(user)
+  	# debugger
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+  	  # debugger
+  	  forget(current_user)
+      session.delete(:user_id)
+      # debugger
+	  @current_user = nil
+  end
+end
