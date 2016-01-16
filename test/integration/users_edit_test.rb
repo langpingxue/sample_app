b86c96906040cd8d62ac89c4a23d2a00c6363277
langpingxue <langping.xue@activenetwork.com>
2016-01-16 15:21:24 +0800

    cannot pass the test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,6 +9,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+  	log_in_as(@user)
     get edit_user_path(@user)
     patch user_path(@user), user: { name:  '',
                                     email: 'foo@invalid',
@@ -18,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+  	log_in_as(@user)
     get edit_user_path(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
@@ -31,4 +33,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal @user.name,  name
     assert_equal @user.email, email
   end
+
+  # error occured
+  # test "successful edit with friendly forwarding" do
+  #   get edit_user_path(@user)
+  #   log_in_as(@user)
+  #   assert_redirected_to edit_user_path(@user)
+  #   name  = "Foo Bar"
+  #   email = "foo@bar.com"
+  #   patch user_path(@user), user: { name:  name,
+  #                                   email: email,
+  #                                   password:              "",
+  #                                   password_confirmation: "" }
+  #   assert_not flash.empty?
+  #   # debugger
+  #   assert_redirected_to @user
+  #   @user.reload
+  #   assert_equal @user.name,  name
+  #   assert_equal @user.email, email
+  # end
 end
